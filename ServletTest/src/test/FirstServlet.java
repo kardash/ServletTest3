dcb3b17eb3cfc3265eb9f627608455c3da4e6384
User <User@PC>
2015-07-14 11:40:18 +0300

    dsfsdfdsfd

--- ServletTest/src/test/FirstServlet.java
+++ ServletTest/src/test/FirstServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/servlet")
 public class FirstServlet extends HttpServlet {
  
     @Override
