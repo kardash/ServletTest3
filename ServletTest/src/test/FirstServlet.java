0a28a7cc752fc377cf5962c4b3eb1ce5824c5679
User <User@PC>
2015-07-14 11:57:38 +0300

    comit

--- ServletTest/src/test/FirstServlet.java
+++ ServletTest/src/test/FirstServlet.java
@@ -17,7 +17,7 @@ public class FirstServlet extends HttpServlet {
             throws ServletException, IOException {
  
         PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello THIS IS Servlet!!!!</h1>")
+        out.print("<h1>Hello THIS IS Servlet!!!!</h1>");
  
     }
 
